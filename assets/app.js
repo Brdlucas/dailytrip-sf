fc35d15212cb4ca218291740e43e35d5e2f174e9
LucasPort <lb.lucasbourdon@gmail.com>
2025-01-21 15:33:37 +0100

    mise à jour des imports dans app.js pour inclure les nouvelles librairies et le fichier style.css

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,16 @@
-import './bootstrap.js';
+import "./bootstrap.js";
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import "./styles/style.css";
+import "./styles/app.css";
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+// Nouvelle librairies et fonctions
+import "./js/splide.min.js";
+import "./js/glightbox.js";
+import "./js/functions.js";
+
+console.log("This log comes from assets/app.js - welcome to AssetMapper! 🎉");
