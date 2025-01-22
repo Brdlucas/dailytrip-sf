a916c054a5605b3e350ef047b1502739c3302ceb
LucasPort <lb.lucasbourdon@gmail.com>
2025-01-22 10:25:57 +0100

    création du contrôleur de page avec les routes pour l'accueil, le contact, les CGU et le RGPD

---
+++ src/Controller/PageController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PageController extends AbstractController
+{
+
+    // Page d'accueil
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function home(): Response
+    {
+        return $this->render('page/home.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    // Page de contact
+    #[Route('/contact', name: 'app_contact', methods: ['GET', 'POST'])]
+    public function contact(): Response
+    {
+        return $this->render('page/contact.html.twig', [
+            'controller_name' => 'ContactController',
+        ]);
+    }
+
+    // page cgu
+    #[Route('/cgu', name: 'app_cgu', methods: ['GET'])]
+    public function cgu(): Response
+    {
+        return $this->render('page/cgu.html.twig', [
+            'controller_name' => 'CguController',
+        ]);
+    }
+
+    // page rgpd
+    #[Route('/rgpd', name: 'app_rgpd', methods: ['GET'])]
+    public function rgpd(): Response
+    {
+        return $this->render('page/rgpd.html.twig', [
+            'controller_name' => 'RgpdController',
+        ]);
+    }
+}
